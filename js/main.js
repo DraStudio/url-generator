dd62d15d6384700bd59372f61fda1e842a93254f
Oscar Cortez <oscar@drastudio.com>
2013-04-08 19:25:16 -0400

    Updated the generated URL to GitHub's new .io scheme
    Thanks Mark!
    https://github.com/blog/1452-new-github-pages-domain-github-io

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //THANKS:http://stackoverflow.com/questions/4006991/build-url-from-form-fields-with-javascript-or-jquery
 $('#ghpagesForm').change(function() {
-	var newURL = "http://" + $('#userName').val() + ".github.com/" + $('#repoName').val() + "/" + $('#dirLocation').val();
+	var newURL = "http://" + $('#userName').val() + ".github.io/" + $('#repoName').val() + "/" + $('#dirLocation').val();
 	$('#contactURL span').empty();
 	$('#contactURL span').append('<a target="_blank" href="' + newURL + '">' + newURL + '</a>');
 });
