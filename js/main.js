d57e33b246ab61e1e85b22ab8dc2c40e60158b61
Oscar Cortez <oscar@drastudio.com>
2013-08-21 12:27:50 -0400

    Added a message indicating GitHub's updated settings area.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 //THANKS:http://stackoverflow.com/questions/4006991/build-url-from-form-fields-with-javascript-or-jquery
 $('#ghpagesForm').change(function() {
 	var newURL = "http://" + $('#userName').val() + ".github.io/" + $('#repoName').val() + "/" + $('#dirLocation').val();
+	var directURL = "https://github.com/" + $('#userName').val() + "/" + $('#repoName').val() + "/settings"
 	$('#contactURL span').empty();
 	$('#contactURL span').append('<a target="_blank" href="' + newURL + '">' + newURL + '</a>');
+	$('#directSettings span').empty();
+	$('#directSettings span').append('<a target="_blank" href="' + directURL + '">Settings</a>');
 });
